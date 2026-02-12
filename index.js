afaca41321af3e2f582b905c87a0b7f640a323de
Lakshay Gambhir <lakshaygambhir96@gmail.com>
2026-02-12 19:08:28 +0530

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login',(req,res) => {
+    res.send('Please login')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
